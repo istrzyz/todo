2a9faf75d5ea88b5fc02e48e60cef28849b5fc78
Ian Strzyzewski <istrzyz@gmail.com>
2016-01-05 18:11:19 -0800

    checkpoint 6

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -3,4 +3,12 @@ class Api::ListsController < ApiController
 
   def create
   end
+
+  def update
+    list = List.find(params[:id])
+    if list.update(list_params)
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages} , status: :unprocessable_entity
+    end
 end
